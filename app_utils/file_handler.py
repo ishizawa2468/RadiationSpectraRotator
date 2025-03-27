687506dff3821ce939b618325164278fad66beae
Osamu Ishizawa <kiaydr@gmail.com>
2025-03-28 08:48:55 +0900

    refactor: set_folderをメソッド分割

--- app_utils/file_handler.py
+++ app_utils/file_handler.py
@@ -8,7 +8,7 @@ from log_util import logger
 class FileHander:
     @staticmethod
     def get_file_list_with_OD(path_to_files, files):
-        logger.debug('OD付きのSPEファイルリストを取得開始')
+        logger.debug('OD付きのSPEファイルリストを取得開始 ->')
         spe_list = []
         spe_display_data = []
         for file in files:
@@ -22,7 +22,7 @@ class FileHander:
             except Exception as e:
                 OD = None
             spe_display_data.append({"File Name": spe.file_name, "OD": OD})
-        logger.debug('終了')
+        logger.debug('-> 終了')
         return pd.DataFrame(spe_display_data)
 
     @staticmethod
@@ -32,7 +32,7 @@ class FileHander:
             rotate_option,
             file_extention = '.spe'
     ):
-        logger.debug('回転後のファイル名の取得を開始')
+        logger.debug('回転後のファイル名の取得を開始 ->')
         rotated_files = []
         for file in files:
             new_file_name = "_".join( # アンスコで要素をつなげる
@@ -43,7 +43,7 @@ class FileHander:
                 ]
             )
             rotated_files.append(new_file_name + file_extention)
-        logger.debug('終了')
+        logger.debug('-> 終了')
         return rotated_files
 
 def get_rotate_deg_str(rotate_deg):
