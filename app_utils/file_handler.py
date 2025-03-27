9d417a9222680945c9a1ca34967e94b7bca67576
Osamu Ishizawa <kiaydr@gmail.com>
2025-03-28 08:45:08 +0900

    refactor: log出力の追加

--- app_utils/file_handler.py
+++ app_utils/file_handler.py
@@ -3,10 +3,12 @@ import os
 import pandas as pd
 
 from modules.file_format.spe_wrapper import SpeWrapper
+from log_util import logger
 
 class FileHander:
     @staticmethod
     def get_file_list_with_OD(path_to_files, files):
+        logger.debug('OD付きのSPEファイルリストを取得開始')
         spe_list = []
         spe_display_data = []
         for file in files:
@@ -20,6 +22,7 @@ class FileHander:
             except Exception as e:
                 OD = None
             spe_display_data.append({"File Name": spe.file_name, "OD": OD})
+        logger.debug('終了')
         return pd.DataFrame(spe_display_data)
 
     @staticmethod
@@ -29,17 +32,18 @@ class FileHander:
             rotate_option,
             file_extention = '.spe'
     ):
-
+        logger.debug('回転後のファイル名の取得を開始')
         rotated_files = []
         for file in files:
-            new_file_name = "_".join(
+            new_file_name = "_".join( # アンスコで要素をつなげる
                 [
                     file[:-4], # 拡張子を取り除く
-                    rotate_option,
-                    get_rotate_deg_str(rotate_deg)
+                    rotate_option, # 回転中心
+                    get_rotate_deg_str(rotate_deg) # 回転角度
                 ]
             )
             rotated_files.append(new_file_name + file_extention)
+        logger.debug('終了')
         return rotated_files
 
 def get_rotate_deg_str(rotate_deg):
--- pages/set_folder.py
+++ pages/set_folder.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 import app_utils.setting_handler as setting_handler
 from app_utils.file_handler import FileHander
-from modules.file_format.spe_wrapper import SpeWrapper
+from log_util import logger
 
 # 共通の設定
 setting_handler.set_common_setting()
@@ -15,38 +15,40 @@ setting = setting_handler.Setting()
 
 # ここから画面と操作
 st.title("📂Set folder")
+logger.info('Set Folder画面')
 
 st.divider()
 
-st.subheader("読み込むフォルダ")
+st.subheader("読み込み先フォルダの設定")
 st.markdown(
     """
-    - ここで設定したフォルダから`.spe`ファイルを選択できます。
-        - Macの場合、Finderでフォルダを選択して `option + command + c`
-        - Windowsの場合、エクスプローラーでフォルダを選択して `shift + control + c`
-    - オリジナルのファイルは読み込むのみで変更されません。回転後のファイルは新しく作られます。
+    - ここで設定した **フォルダ** から`.spe`ファイルを取得できます
+    - オリジナルのファイルは読み込むのみで変更されません。回転後のファイルは新しく作られます
     """
 )
-read_path = st.text_input(label='オリジナルの`.spe`があるフォルダまでのfull path', value=setting.setting_json['read_path'])
-if st.button('読み込み先を更新'):
+read_path = st.text_input(label='`.spe`があるフォルダまでのfull path', value=setting.setting_json['read_path'])
+if st.button('読み込み先を更新', type='primary'):
     setting.update_read_spe_path(read_path)
+    logger.info(f'読み込み先を更新: {read_path}')
 
 st.subheader("見つかったFiles")
 setting = setting_handler.Setting() # オブジェクトを作り直して読み込み直す
-# FIXME: 関数化
 path_to_files = setting.setting_json['read_path'] # 別ページで設定した読み込みpathを取得
 # ファイルが得られるpathかどうか確認
 try:
     files = os.listdir(path_to_files)
     if not any(file.endswith('.spe') and not file.startswith('.') for file in files):
         st.write(f'有効なファイルが {path_to_files} にありません。')
+        logger.info('ファイルが無いパスが設定された')
         st.stop()
 except Exception as e:
     st.subheader('Error: pathが正しく設定されていません。ファイルが存在するフォルダを指定してください。')
     st.subheader('現在の設定されているpath: {}'.format(path_to_files))
+    logger.info('存在しないパスが設定された')
     st.stop() # 以降の処理をしない
 
 # ファイルが見つかった場合
+logger.info('ファイルが見つかった')
 files.sort() # 見やすいようにソートしておく
 filtered_files = [] # .speで終わるもののみを入れるリスト
 for file in files:
@@ -54,7 +56,7 @@ for file in files:
         filtered_files.append(file)
 # 一通り終わったら、filesを置き換える
 files = filtered_files
-# 表示
+# ODごとに表示
 spe_display_data = FileHander.get_file_list_with_OD(path_to_files, files)
 for od in (set(spe_display_data['OD'])):
     st.table(spe_display_data[spe_display_data['OD'] == od])
@@ -62,8 +64,8 @@ for od in (set(spe_display_data['OD'])):
 
 st.divider()
 
-st.subheader("保存先フォルダ")
+st.subheader("保存先フォルダの設定")
 st.write("- ここで設定したフォルダに回転された`.spe`ファイルが保存されます。")
 save_path = st.text_input(label='保存フォルダまでのfull path', value=setting.setting_json['save_path'])
-if st.button('保存先を更新'):
+if st.button('保存先を更新', type='primary'):
     setting.update_save_spe_path(save_path)
